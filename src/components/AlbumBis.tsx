a6fc833519c12050ad1118237c8311e16a29379e
Constantin <istrateconstantin6@gmail.com>
2023-06-29 11:01:18 +0200

    Tickets [13] & [4] finish

--- src/components/AlbumBis.tsx
+++ src/components/AlbumBis.tsx
@@ -1,29 +1,18 @@
 import { Card } from 'primereact/card';
 import { Image } from 'primereact/image';
-import { DataView, DataViewLayoutOptions, DataViewLayoutType  } from 'primereact/dataview';
-import { Paginator  } from 'primereact/paginator';
+import { DataViewLayoutType } from 'primereact/dataview';
 import './Album.css';
+
 function AlbumBis(item: any, layout: DataViewLayoutType) {
     
     function getImage(item: any) {
         const image = item.images.filter( (i: { height: number; }) => i.height === 64)[0];
         return image.url;
     }
-
-    // return (
-    //     <div className='cardsContainer'>
-    //         <Card title={item.name} subTitle={item.release_date} style={{width:'33.33%'}}>
-    //             <Image src={getImage(item)} alt={item.name}></Image>
-    //         </Card>
-    //     </div>
-    // )
     return (
-        <div className='cardsContainer'>
-            <p className='card'>Hello</p>
-        </div>
+        <Card title={item.name} subTitle={item.release_date} >
+            <Image src={getImage(item)} alt={item.name}></Image>
+        </Card>
     )
-
 }
-
-
 export default AlbumBis
--- src/components/AlbumListBis.tsx
+++ src/components/AlbumListBis.tsx
@@ -2,23 +2,14 @@ import { useEffect, useState } from "react";
 import Album from "./Album";
 import AlbumBis from "./AlbumBis";
 import { DataView,   } from 'primereact/dataview';
-import { Paginator  } from 'primereact/paginator';
 import './AlbumList.css';
-
-    const dataviewStyle = {
-        width: '100%',
-        height: '100vh',
-        // backgroundColor: 'green',
-       
-        //? display flex concern unchangeable div above this container and the yellowContainer
-    };
-    
+   
     function AlbumListBis({ albums }: any) {
         
         return (
 
             <div className="albumsBis" style={{ display: "flex", justifyContent: "center" }}>
-                <DataView value={albums} itemTemplate={AlbumBis} style={dataviewStyle} layout="list" paginator rows={5} />
+                <DataView value={albums} itemTemplate={AlbumBis} layout="grid" paginator rows={12} />
             </div>
         )
 }
