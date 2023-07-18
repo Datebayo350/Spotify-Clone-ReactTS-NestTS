7f2043006ea3aa61f7a26c2a93d9a92737c9bc48
Constantin <61415930+Datebayo350@users.noreply.github.com>
2023-07-18 21:34:24 +0200

    RTK Query api slice created and configured

--- src/__slices/api.ts
+++ src/__slices/api.ts
@@ -1,3 +1,27 @@
-import { createApi } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const api = createApi()
+export const apiSlice = createApi({
+    reducerPath: 'backendApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:8080',
+    }),
+    endpoints: builder => ({
+        spotifyRefreshAccessToken: builder.query({
+            query: () => '/spotify-login/refresh-token'
+        }),
+        updateUserInformations: builder.mutation({
+            query: () => {
+                return {
+                    url:"/user/me/update",
+                    method: "POST",
+                    body: {
+                        username: 'Nouveau Identifiant'
+                    }
+                }
+            }
+        }),
+    })
+    
+});
+
+export const { useSpotifyRefreshAccessTokenQuery } = apiSlice;
