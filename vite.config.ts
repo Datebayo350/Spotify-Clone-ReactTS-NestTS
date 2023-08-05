dd8bcd6defcc039dda39f4f2e85f9df4867e2090
Constantin <61415930+Datebayo350@users.noreply.github.com>
2023-08-05 18:01:56 +0200

    Docker compose configuration file created and set-up, vite config file updated to enable live reload into docker application for development mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// https://rollupjs.org/configuration-options/#watch
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 3000,
+    hmr: {
+      host: '0.0.0.0',
+      clientPort: 3000,
+    },
+    //  watch property options and their descriptions : https://github.com/paulmillr/chokidar#api
+    watch: {
+      usePolling: true,
+      interval: 0,
+    },
   },
 });
